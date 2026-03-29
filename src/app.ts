37981c5f5c84c471b7e24bb48fe9df5df076e52a
RamosTomas <tomasramos138@gmail.com>
2026-03-29 20:00:40 -0300

    corsOk

--- src/app.ts
+++ src/app.ts
@@ -20,26 +20,20 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const app = express()
-
-app.use(cors({
-  origin: process.env.FRONT_URL || 'http://localhost:5173',
-  allowedHeaders: ['Authorization', 'Content-Type'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH' ,'OPTIONS'],
-  credentials: true
-}));
-app.options('*', cors()); 
-
 app.use(express.json())
 
 app.use((req, res, next) => {
   RequestContext.create(orm.em, next)
 })
+app.use(cors());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+//Esto permite servir archivos estáticos desde la carpeta "public"
 app.use('/public', express.static(path.join(process.cwd(), 'public')));
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+
 
 app.use("/api/auth", authRouter)
 app.use('/api/cliente', clienteRouter)
@@ -49,7 +43,7 @@ app.use('/api/zona', ZonaRouter)
 app.use('/api/categoria', CategoriaRouter)
 app.use('/api/venta', VentaRouter)
 app.use('/api/item-venta', ItemVentaRouter)
-app.use('/api/mercadopago', mercadoPagoRouter)
+app.use ('/api/mercadopago', mercadoPagoRouter)
 
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resource not found' })
@@ -63,5 +57,6 @@ const server = app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`);
 });
 
+// Exporta la app para testing
 export default app
 export { app }
